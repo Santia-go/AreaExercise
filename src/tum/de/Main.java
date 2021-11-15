c8022e54332e752404abeca21827e05901778855
Santiago Berrezueta <>
2021-11-15 18:17:17 +0100

    First Exercise - Area

--- src/tum/de/Main.java
+++ src/tum/de/Main.java
@@ -4,7 +4,7 @@ class Shape {
     protected String color;
     public double area() {
         double area = 0.0;
-        //TODO: calculation of the area
+        //TODO: calculation of the area of any figure
         return area;
     }
 
